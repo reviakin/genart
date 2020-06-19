30a69cfac01a97dc083cbee1f4a57954558b21f7
reviakin <d.reviakin@8bridge.org>
2020-06-19 22:49:48 +0200

    mix color

--- shader.js
+++ shader.js
@@ -16,7 +16,10 @@ const frag = glsl(/* glsl */ `
   varying vec2 vUv;
 
   void main () {
-    vec3 color = vec3(sin(time) + 1.0);
+    vec3 colorA = vec3(2.0, 1.0, 0.8);
+    vec3 colorB = vec3(0.0, 1.0, 0.0);
+
+    vec3 color = mix(colorA, colorB, vUv.x);
     gl_FragColor = vec4(color, 1.0); 
   }
 `);
